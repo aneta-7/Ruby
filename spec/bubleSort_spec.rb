f0314388285365bb102191baa9c1f8a3f54afa77
aneta-7 <aneta.stypa7@wp.pl>
2015-11-17 14:45:36 +0100

    commit4

---
+++ spec/bubleSort_spec.rb
@@ -0,0 +1,12 @@
+require 'simplecov'
+SimpleCov.start
+
+require_relative '../lib/bubleSort'
+
+RSpec.describe 'bubleSort.rb -- Enumerable' do
+  it '#sort_by_freq is defined'
+
+  it '#sort_distinct_by_freq'
+
+  # tutaj wpisac pozostale testy
+end
