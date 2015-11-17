01ccd10a3752ea87064039c4760d70730a4b6212
aneta-7 <aneta.stypa7@wp.pl>
2015-11-17 14:03:41 +0100

    commit

---
+++ lib/insertSort.rb
@@ -0,0 +1,13 @@
+class InsertionSort
+    def sort_out_of_place(to_sort)
+        sorted = []
+
+        to_sort.each do |element|
+            for index in 0..(to_sort.length - 1)
+                sorted.insert(index, element) if to_sort[index] > element
+            end
+        end
+
+        return to_sort
+    end
+end
