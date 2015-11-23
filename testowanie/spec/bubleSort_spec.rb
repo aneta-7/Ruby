48a95a10f0618f70aa8b2394d99ea5149eae465b
aneta-7 <aneta.stypa7@wp.pl>
2015-11-23 21:57:28 +0100

    commit

--- testowanie/spec/bubleSort_spec.rb
+++ testowanie/spec/bubleSort_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe 'bubleSort.rb' do
 
   it "should sort an unsorted array" do
       expect(bubble_sort(arr)).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-      expect(bubble_sort(random_arr)).to eq(sorted_random_arr)
       expect(bubble_sort([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
       expect(bubble_sort([1, 2, 3, 4, 5, 0, 4, 3, 2, 1])).to eq([0, 1, 1, 2, 2, 3, 3, 4, 4, 5])
+      expect(bubble_sort(random_arr)).to eq(sorted_random_arr)
   end
 
   it "should return an empty array, when given one" do
--- testowanie/spec/mergeSort_spec.rb
+++ testowanie/spec/mergeSort_spec.rb
@@ -6,9 +6,13 @@ require_relative '../lib/mergeSort'
 RSpec.describe 'mergeSort.rb' do
   describe '#mergeSort' do
 
+    let(:random_arr) { Array.new(10) { rand(100) } }
+    let(:sorted_random_arr) { random_arr.sort }
+
     it "should sort an unsorted array" do
         expect(merge_sort([1, 3, 5, 7, 9, 8, 6, 4, 2, 0])).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         expect(merge_sort([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        expect(merge_sort(random_arr)).to eq(sorted_random_arr)
         expect(merge_sort([1, 2, 3, 4, 5, 0, 4, 3, 2, 1])).to eq([0, 1, 1, 2, 2, 3, 3, 4, 4, 5])
     end
 
